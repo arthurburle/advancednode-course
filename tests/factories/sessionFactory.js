db4fce9f5ec43e94e19bc123e6b3d77b043437cd
ArthurBurle <arthurburle@gmail.com>
2021-02-01 14:42:22 -0300

    keygrip fixed (I think)

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -1,7 +1,7 @@
 const Buffer = require('safe-buffer').Buffer;
-const Keygrip = require('Keygrip');
-// const keys = require('../../config/keys');
-const keygrip = new Keygrip(['123123123']);
+const Keygrip = require('keygrip');
+const keys = require('../../config/keys');
+const keygrip = new Keygrip([keys.cookieKey]);
 
 module.exports = user => {
   const sessionObject = {
